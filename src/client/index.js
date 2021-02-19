84daae4237995b772c5dc98571967527f185f270
ImmanuelXIV <manuel.schwall@gmail.com>
2021-02-19 14:10:08 +0100

    feat: Remove nameChecker.js

--- src/client/index.js
+++ src/client/index.js
@@ -3,19 +3,14 @@ import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
-import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 import { postData } from './js/postData'
 import { updateEntry } from './js/updateEntry'
 
 
-console.log(checkForName);
-
 alert("I EXIST")
-console.log("CHANGE!!");
 
 export {
-    checkForName,
     handleSubmit,
     postData,
     updateEntry
